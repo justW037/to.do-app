af28df53380feef633a26a3e833a485469864e68
JustW <vuav2003@gmail.com>
2023-07-04 17:46:42 +0700

    .

--- lib/colors.dart
+++ lib/colors.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 
 
 class AppColor{
-  static const Color PrimaryColor = Color(0xFFF9E9C8);
+  static Color PrimaryColor = Color(0xFFF9E9C8);
   static const Color PrimaryColor2 = Color(0xFFDEECED);
   static const Color PrimaryColor3 = Color(0xFFEDDAB3);
+}
+
+
+class Heading{
+  static const TextStyle Heading1 =TextStyle(fontSize: 40, fontWeight: FontWeight.w500 );
+  static const TextStyle Heading2 = TextStyle(fontSize: 30, fontWeight: FontWeight.w400);
+  static const TextStyle Heading3 =TextStyle(fontSize: 20, fontWeight: FontWeight.w500,color: Colors.black );
 }
